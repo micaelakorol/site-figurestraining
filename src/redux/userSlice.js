6e55f86380862657e7fc8abab4e3b7cde8c96fdb
Micaela Korol <micaelakorol@gmail.com>
2024-08-09 12:35:46 -0300

    svg google deleted

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,20 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialStateUser = {
-  displayName: '',
-  email: '',
+  displayName: "",
+  email: "",
   emailVerified: false,
   isLogged: false,
-  uid: '',
-  photoURL: ''
+  uid: "",
+  photoURL: "",
 };
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: initialStateUser,
   reducers: {
     currentUser: (state, action) => {
-      const { email, emailVerified, uid, displayName, photoURL } = action.payload;
+      const { email, emailVerified, uid, displayName, photoURL } =
+        action.payload;
       if (email !== undefined) {
         state.email = email;
       }
@@ -32,10 +33,10 @@ export const userSlice = createSlice({
       }
     },
     userIsLogged: (state, action) => {
-      const {isLogged} = action.payload;
+      const { isLogged } = action.payload;
       state.isLogged = isLogged;
     },
-  }
+  },
 });
 
 export const { currentUser, userIsLogged } = userSlice.actions;
